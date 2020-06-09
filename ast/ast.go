b7c9593a2435c2a6f34bc9da5e1645ee4db9192d
pjserol <pjserol@gmail.com>
2020-06-09 21:26:43 +1000

    Revert "add parsing return statement"
    This reverts commit 28191a8f07b538509dbf1f001aa7852e8994aedf.

--- ast/ast.go
+++ ast/ast.go
@@ -39,7 +39,8 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) statementNode() {
+}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
@@ -52,15 +53,4 @@ type Identifier struct {
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
-}
-
-type ReturnStatement struct {
-	Token       token.Token // the `return` token
-	ReturnValue Expression
-}
-
-func (rs *ReturnStatement) statementNode() {}
-
-func (rs *ReturnStatement) TokenLiteral() string {
-	return rs.Token.Literal
 }
