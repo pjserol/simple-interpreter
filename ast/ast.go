c95346cebd7d322d55d2af233ba32b978a6cee36
pjserol <pjserol@gmail.com>
2020-06-09 21:13:48 +1000

    add parsing return expression

--- ast/ast.go
+++ ast/ast.go
@@ -39,8 +39,7 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode() {
-}
+func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
@@ -53,4 +52,15 @@ type Identifier struct {
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+type ReturnStatement struct {
+	Token       token.Token // the `return` token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
 }
